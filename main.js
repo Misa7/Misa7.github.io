e76efdce3abda05b1b355dd2cd0f10da9060bef6
Misa7 <ma.ducros7@gmail.com>
2019-01-04 09:32:17 +0100

    add window

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 function confirmSite() {
 if (confirm("Veux-tu aller sur xxxvidsxxx ?")){
-<a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"> window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ");</a>}
+location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ" window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ");}
 };
 
 setTimeout(function(){ confirm("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please"); }, 10000);
