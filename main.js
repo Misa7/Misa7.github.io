02b02b66c0445886f81cd436011fe32bc3901096
Misa7 <ma.ducros7@gmail.com>
2019-01-04 08:59:41 +0100

    confirm + timer

--- main.js
+++ main.js
@@ -1,6 +1,4 @@
+function confirmSite() {
+if (confirm("Veux-tu aller sur xxxvidsxxx ?")){
+location.href = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'}}
 setTimeout(function(){ confirm("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please"); }, 10000);
-function confirmWebsite() {
-  var theAnswer = confirm("Veux-tu aller sur xxxvidsxxx ?");
-  if (theAnswer) {
-    <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" onclick="window.open(https://www.youtube.com/watch?v=dQw4w9WgXcQ)" </a>;
-  }
