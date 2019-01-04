519f1f4f83eca761193e1d8859e38b7e2ba75f19
Misa7 <ma.ducros7@gmail.com>
2019-01-04 18:00:37 +0100

    sans window

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 function confirmSite() {
 if (confirm("Veux-tu aller sur xxxvidsxxx ?")){
-location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"}
+location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";}
 };
+
 setTimeout(function(){ confirmSite("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please"); }, 10000);
