a5181efd0f272161494049dee2215902b1fcf1ae
Misa7 <ma.ducros7@gmail.com>
2019-01-04 08:34:10 +0100

    m

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
-setTimeout(function(){ alert("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please"); }, 10000);
+setTimeout(function(){ confirm("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please"); }, 10000);
 function myFunction() {
-  var txt;
-  var r = confirm("Veux-tu aller sur xxxvidsxxx ?");
-  if (r == true) {
-    <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" onclick="window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+  var theAnswer = confirm("Veux-tu aller sur xxxvidsxxx ?");
+  if (theAnswer) {
+    <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" onclick="window.open(https://www.youtube.com/watch?v=dQw4w9WgXcQ)"> </a>;
   }
