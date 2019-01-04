45c810e7a8375bbb8b1001377efe0857637d2c96
Misa7 <ma.ducros7@gmail.com>
2019-01-04 09:02:41 +0100

    l

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
+setTimeout(function(){ confirm("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please"); }, 10000);
 function confirmSite() {
 if (confirm("Veux-tu aller sur xxxvidsxxx ?")){
 location.href = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'}}
-setTimeout(function(){ confirm("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please"); }, 10000);
