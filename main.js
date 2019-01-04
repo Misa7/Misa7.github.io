8c4ffc2e118279e3eb1a31fa20818c31b612aa51
Misa7 <ma.ducros7@gmail.com>
2019-01-04 09:07:39 +0100

    m

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 setTimeout(function(){ confirm("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please"); }, 10000);
-function confirmSite() {
+function confirm() {
 if (confirm("Veux-tu aller sur xxxvidsxxx ?")){
-location.href = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'}}
+<a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"></a>}
+}
