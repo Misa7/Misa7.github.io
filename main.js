ca09bb1777f3fe5cf2e427f3080bee5fc252e852
Misa7 <ma.ducros7@gmail.com>
2019-01-04 08:21:31 +0100

    popup

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
 setTimeout(function(){ alert("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please"); }, 10000);
+function myFunction() {
+  var txt;
+  var r = confirm("Veux-tu aller sur xxxvidsxxx ?");
+  if (r == true) {
+    <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" onclick="window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+  }
