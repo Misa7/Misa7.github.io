8fd6685b08f6fcdac272a196374cb36e726e1091
Misa7 <ma.ducros7@gmail.com>
2019-01-04 09:17:33 +0100

    ajout timeout 2

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 function confirmSite() {
 if (confirm("Veux-tu aller sur xxxvidsxxx ?")){
-location.href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">}
-}
+location.href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"}
+};
 setTimeout(function(){ confirm("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please"); }, 10000);
